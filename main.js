4c4b17b87977e16b4322745218572b96949f93c9
Ryan <rolearyprogramming@gmail.com>
2024-06-06 16:55:22 -0600

    feature/ modified javascript so that wins are kept track of on the side bars

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-var rpsThrow = {
-    playerChoice: null,
-    opponentChoice: null
-}
 
-var randomChoiceHelperArray = ['rock', 'paper', 'scissors', 'lizard', 'alien', 'ufo'];
-
+var currentGame;
 var currentGameType;
 var playerCurrentWins;
 var opponentCurrentWins;
-
 var savedPlayers = [];
+var choiceButtons = [];
+
+
 var currentPlayers = {
     player1: null,
     player2: null,
 };
-choiceButtons = [];
-
-
-matchupArray = [
+var rpsThrow = {
+    playerChoice: null,
+    opponentChoice: null
+}
+var matchupArray = [
     // 0 = draw, 1 = win, 2 = loss
     [0, 2, 1, 1, 2, 0], [1, 0, 2, 0, 1, 2], [2, 1, 0, 2, 0, 1], [2, 0, 1, 0, 1, 2], [1, 2, 0, 2, 0, 1], [0, 1, 2, 1, 2, 0]
 ]
+var randomChoiceHelperArray = ['rock', 'paper', 'scissors', 'lizard', 'alien', 'ufo'];
 
 
 
@@ -35,6 +34,9 @@ function onLoad() {
         choiceButtons[j].isLocked = false;
         choiceButtons[j].addEventListener('click', setPlayerChoice);
     }
+    currentPlayers.player1 = createPlayer('You', '🐶', 0);
+    currentPlayers.player2 = createPlayer('Computer', '🤖', 0);
+    currentGame = createGame(currentPlayers.player1, currentPlayers.player2);
 }
 onLoad();
 
@@ -48,8 +50,12 @@ function createPlayer(playerName = 'human', playerToken = '👩🏻', playerWins
     return player;
 }
 
+function chooseGameType(){
 
-function createGame(p1, p2, gameType) {
+}
+
+
+function createGame(p1, p2, gameType = 'advanced') {
     gameObject =
     {
         player1: p1,
@@ -75,8 +81,13 @@ function setPlayerChoice() {
     makeOpponentChoice();
     resolveThrow();
 }
-function sideBarDisplay() {
 
+function sideBarDisplay() {
+    var playerSide = document.querySelector('.player-area');
+    var opponentSide = document.querySelector('.opponent-area');
+    playerSide.innerHTML = `<div>Wins:${currentGame.player1.wins}</div>`
+    opponentSide.innerHTML = `<div>Wins:${currentGame.player2.wins}</div>`
+    
 }
 
 function resolveThrow() {
@@ -89,9 +100,13 @@ function resolveThrow() {
     }
     else if (resolutionVal === 1) {
         console.log("YOU WIN!");
+        currentGame.player1.wins += 1;
+        sideBarDisplay();
     }
     else {
         console.log("You lose...");
+        currentGame.player2.wins += 1;
+        sideBarDisplay();
     }
 
 }
